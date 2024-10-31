963252e3d1006c14b1a20d708831bacf30304872
ashishkarche <ashishkarche9@gmail.com>
2024-10-31 23:23:28 +0530

    v6

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000;
 const db = require('./config/db');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+}));
 app.use(express.json());
 
 // Routes
