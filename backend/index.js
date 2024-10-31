ffb08dfc78b1b3ed19152dc69e86b3a95be55a78
ashishkarche <ashishkarche9@gmail.com>
2024-11-01 00:16:03 +0530

    FV

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,7 @@ const PORT = process.env.PORT || 5000;
 const db = require('./config/db');
 
 // Middleware
-app.use(cors({
-    origin:[""],
-    methods:["POST","GET"],
-    credentials:true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
